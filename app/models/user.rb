d9b2fc544af18bb1161ae41386d0c0cdf8a20cf0
tallan groberg <tallan.taven@gmail.com>
2019-06-01 12:56:13 -0600

    center name in trips index

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,9 @@
 class User < ApplicationRecord
+
+  def full_name
+    "#{self.first_name}  #{self.last_name}"
+
+  end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
