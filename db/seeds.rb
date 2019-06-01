7860c0daac113d607871ed1b799ba8617413c810
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-01 11:14:53 -0600

    navbar logic

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,22 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+u = User.create(
+  email: "test@test.com",
+  password: "password",
+  password_confirmation: "password"
+)
+3.times do 
+  trip = Trip.create(
+    name: Faker::TvShows::StarTrek.location,
+    start_date: Faker::Date.between(2.days.ago, Date.today),
+    end_date: Faker::Date.forward(3)
+    trip_id: u.id
+  )
+  2.times do 
+    Location.create(
+      name: Faker::,
+      fees: ,
+      image:,
+
+    )
+    
+  end
+end
