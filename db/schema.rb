2bbabfa08a346a9da7656b80f936872eeb403e6e
tallan groberg <tallan.taven@gmail.com>
2019-06-01 12:00:32 -0600

    updated scema after db:drop

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,7 @@
 
 ActiveRecord::Schema.define(version: 2019_06_01_164146) do
 
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "addresses", force: :cascade do |t|
