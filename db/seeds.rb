2cb055efaf3a1507c596f936db36c55a23846da0
Patrick Lamoureux <patrick@blackcedar.com>
2019-06-01 11:37:14 -0600

    seed file works yay

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,29 +3,34 @@ u = User.create(
   password: "password",
   password_confirmation: "password"
 )
-3.times do 
-  trip = Trip.create(
-    name: Faker::TvShows::StarTrek.location,
-    start_date: Faker::Date.between(2.days.ago, Date.today),
-    end_date: Faker::Date.forward(3)
-    user_id: u.id
-  )
-  2.times do 
-    Location.create(
-      name: Faker::Address.state,
-      fees: ,
-      image:,
-      trip_id: trip.id
-
+  3.times do 
+    trip = Trip.create(
+      name: Faker::TvShows::StarTrek.specie,
+      start_date: Faker::Date.between(2.days.ago, Date.today),
+      end_date: Faker::Date.forward(3),
+      user_id: u.id
     )
-    2.times do
-      street:Faker::Address.street_address,
-      city:Faker::Address.city,
-      state:Faker::Address.state,
-      lat:Faker::Address.latitude,
-      long:Faker::Address.longitude,
-      location_id: location.id
-    end
+      2.times do 
+      
+      location = Location.create(
+        name: Faker::Address.state,
+        fees: "10 Dollars",
+        image:Faker::Dessert.variety,
+        trip_id: trip.id
+  
+      )
+      
+          Address.create(
+  
+            street:Faker::Address.street_address,
+            city:Faker::Address.city,
+            state:Faker::Address.state,
+            lat:Faker::Address.latitude,
+            long:Faker::Address.longitude,
+            location_id: location.id
+            )
     
   end
 end
+
+puts "Created User with 3 trips 2 locations per trip and 1 address per location "
